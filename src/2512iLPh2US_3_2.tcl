094b33a6b4c9abdfef498953aa1cb55aeae51994
Subinoy <suny.bio@gmail.com>
2024-11-13 01:39:22 +0530

    fEOAD solar scripts

---
+++ src/2512iLPh2US_3_2.tcl
@@ -0,0 +1,18 @@
+proc 2512iLPh2US {} {
+    load pedigree solar_ped.1.csv
+    pedigree show
+    load pheno pheno_solar_US3475.csv
+
+        trait age
+	covar SEX edu APOE2 APOE4 fEOAD_82_solr2 PCA1 PCA2 PCA3
+	polygenic -s -p 0.99   
+    	mibddir /mnt/vast/hpc/lee_lab/LLFS-Data/IBD_haplotype_wgs_gwas_08.25.2022/mibd_grid_cMH_wholenum_even/
+    	chromosome all
+    	interval 1
+    	multipoint
+
+
+
+
+    	quit
+}
